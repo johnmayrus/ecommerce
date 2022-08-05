f6a116d9f302676b3d3610f6e6702012c671bd4b
Alessandro P. Souza <alessandro-design@hotmail.com>
2022-08-05 10:14:00 -0300

    Admin - Produtos busca e paginação

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -47,6 +47,49 @@
             return $this->setdesphoto($url);
         }
         
+        public static function getPage($page = 1, $itensPerPage = 10)
+        {
+            $start = ($page - 1) * $itensPerPage;
+            $sql = new Sql();
+            $results = $sql->select("
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_products
+            ORDER BY desproduct
+            LIMIT $start, $itensPerPage;");
+            
+            $resultsTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+            
+            return [
+                'data' => $results,
+                'total' => (int)$resultsTotal[0]['nrtotal'],
+                'pages' => ceil($resultsTotal[0]['nrtotal'] / $itensPerPage)
+            ];
+        }
+        
+        public static function getPageSearch($search, $page = 1, $itensPerPage = 10)
+        {
+            
+            $start = ($page - 1) * $itensPerPage;
+            $sql = new Sql();
+            $results = $sql->select("
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_products
+            WHERE desproduct LIKE :search
+            ORDER BY desproduct
+            LIMIT $start, $itensPerPage;
+            ", [
+                ':search' => '%' . $search . '%'
+            ]);
+            
+            $resultsTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+            
+            return [
+                'data' => $results,
+                'total' => (int)$resultsTotal[0]['nrtotal'],
+                'pages' => ceil($resultsTotal[0]['nrtotal'] / $itensPerPage)
+            ];
+        }
+        
         public function save()
         {
             $sql = new Sql();
